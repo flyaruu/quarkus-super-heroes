c62fc164fe1a27c93dbbb69e400d9e37d18147d6
Frank Lyaruu <flyaruu@gmail.com>
2025-03-06 16:31:04 +0100

    refactor

--- k6.js
+++ k6.js
@@ -1,8 +1,8 @@
 import http from 'k6/http';
 
 export const options = {
-    vus: 10,
-    duration: '40s'
+    vus: 50,
+    iterations: 10000
 };
 
 export default() => {
@@ -10,7 +10,6 @@ export default() => {
     var hero = response_body.hero;
     var villain = response_body.villain;
     var location = JSON.parse(http.get("http://localhost:8082/api/fights/randomlocation").body);
-
     var fight_request = { hero: hero, villain: villain, location: location};
     var fight_result = JSON.parse(http.post("http://localhost:8082/api/fights", JSON.stringify(fight_request)).body);
     console.log(fight_result.winnerName);
