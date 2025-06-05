c0fd01fec5a9b5f8b7402c2977d138dcd2ea0b2e
Frank Lyaruu <flyaruu@gmail.com>
2025-06-05 21:49:26 +0200

    unify rates

--- k6/randomFight.js
+++ k6/randomFight.js
@@ -1,43 +1,50 @@
 import http from 'k6/http';
 import { check } from 'k6';
 
+const host = __ENV.K6_HOST || "http://localhost:8080"
+
 export function randomFight() {
-  const json_post_header = {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  };
-  var response = http.get("http://localhost:8082/api/fights/randomfighters");
-  check(response, {
-    'random fighters status is 200': (r) => r.status === 200,
-  });
-  var response_body = JSON.parse(response.body);
+    const json_post_header = {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+    var url = host + "/api/fights/randomfighters";
+    // console.log("Requesting random fighters from: " + url);
+    var response = http.get(host + "/api/fights/randomfighters");
+    check(response, {
+        'random fighters status is 200': (r) => r.status === 200,
+    });
+    var body = response.body;
+    // console.log(body);
 
-  var hero = response_body.hero;
-  var villain = response_body.villain;
-  check(location, {
-    'hero is not fallback': (r) => !hero.name.toLowerCase().includes("fallback"),
-    'villain is not fallback': (r) => !villain.name.toLowerCase().includes("fallback")
-  })
+    var response_body = JSON.parse(body);
 
-  var location_response = http.get("http://localhost:8082/api/fights/randomlocation");
-  check(response, {
-    'location status is 200': (r) => r.status === 200,
-  });
-  var location = JSON.parse(location_response.body);
-  check(location, {
-    'location is not fallback': (r) => !location.name.toLowerCase().includes("fallback")
-  })
-  var fight_request = { hero: hero, villain: villain, location: location };
-  var fight_response = http.post("http://localhost:8082/api/fights", JSON.stringify(fight_request), json_post_header);
-  // console.log(fight_response);
-  check(fight_response, {
-    'fight result is 200': (r) => r.status === 200
-  })
-  var fight_result = JSON.parse(fight_response.body);
-  // console.log("===============");
+    var hero = response_body.hero;
+    var villain = response_body.villain;
+    check(location, {
+        'hero is not fallback': (r) => !hero.name.toLowerCase().includes("fallback"),
+        'villain is not fallback': (r) => !villain.name.toLowerCase().includes("fallback")
+    })
 
-  // console.log(fight_result);
+    var location_response = http.get(host + "/api/fights/randomlocation");
+    check(response, {
+        'location status is 200': (r) => r.status === 200,
+    });
+    var location = JSON.parse(location_response.body);
+    check(location, {
+        'location is not fallback': (r) => !location.name.toLowerCase().includes("fallback")
+    })
+    var fight_request = { hero: hero, villain: villain, location: location };
+    var fight_response = http.post(host + "/api/fights", JSON.stringify(fight_request), json_post_header);
+    // console.log(fight_response);
+    check(fight_response, {
+        'fight result is 200': (r) => r.status === 200
+    })
+    var fight_result = JSON.parse(fight_response.body);
+    // console.log("===============");
 
-  return fight_result
+    // console.log(fight_result);
+
+    return fight_result
 }
